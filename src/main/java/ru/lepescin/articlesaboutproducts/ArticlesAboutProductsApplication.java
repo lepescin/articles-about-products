e033cd703ca3cb310408e4cac4b956461c4eb834
Юрий <lepescin@gmail.com>
2021-06-03 16:42:32 +0300

    Initial commit

---
+++ src/main/java/ru/lepescin/articlesaboutproducts/ArticlesAboutProductsApplication.java
@@ -0,0 +1,13 @@
+package ru.lepescin.articlesaboutproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesAboutProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesAboutProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lepescin/articlesaboutproducts/ArticlesAboutProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lepescin.articlesaboutproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesAboutProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
